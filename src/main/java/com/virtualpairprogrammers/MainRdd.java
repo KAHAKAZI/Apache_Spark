9ed6e6ea0a33ddb0be130e99a020729fb511dcdf
Christopher <kahakazi@gmail.com>
2022-10-10 14:09:13 +0200

    Refactor

--- src/main/java/com/virtualpairprogrammers/MainRdd.java
+++ src/main/java/com/virtualpairprogrammers/MainRdd.java
@@ -1,6 +1,5 @@
 package com.virtualpairprogrammers;
 
-import com.google.common.collect.Iterables;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
@@ -10,10 +9,9 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
-public class Main {
+public class MainRdd {
 
     public static void main(String[] args) {
         List<String> inputData = new ArrayList<>();
